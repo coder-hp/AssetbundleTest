e49508973f7d1765f10b3680960f64f17095bf1b
coder-hp <582254172@qq.com>
2023-02-14 11:09:36 +0800

    11

--- AssetBundleTest/Assets/Editor/BuildAssetBundle.cs
+++ AssetBundleTest/Assets/Editor/BuildAssetBundle.cs
@@ -6,11 +6,55 @@ using UnityEngine;
 
 public class BuildAssetBundle : MonoBehaviour
 {
-    [MenuItem("HP Utils/AssetBundle/Build")]
-    static void StartBuildAssetBundles()
+    enum AssetBundlePlatform
+    {
+        PC,
+        Android,
+        iOS,
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Build/PC")]
+    static void BuildPC()
+    {
+        BuildAssetBundles(AssetBundlePlatform.PC);
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Build/Android")]
+    static void BuildAndroid()
+    {
+        BuildAssetBundles(AssetBundlePlatform.Android);
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Build/iOS")]
+    static void BuildIos()
+    {
+        BuildAssetBundles(AssetBundlePlatform.iOS);
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Clear/PC")]
+    static void ClearAssetBundlePC()
+    {
+        ClearBuildAssetBundles(AssetBundlePlatform.PC);
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Clear/Android")]
+    static void ClearAssetBundleAndroid()
+    {
+        ClearBuildAssetBundles(AssetBundlePlatform.Android);
+    }
+
+    [MenuItem("HP Utils/AssetBundle/Clear/iOS")]
+    static void ClearAssetBundleIos()
+    {
+        ClearBuildAssetBundles(AssetBundlePlatform.iOS);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    static void BuildAssetBundles(AssetBundlePlatform assetBundlePlatform)
     {
         // ab资源存储目录
-        string path = Application.dataPath.Replace("/Assets", "/AssetBundles/PC");
+        string path = Application.dataPath.Replace("/Assets", "/AssetBundles/" + assetBundlePlatform.ToString());
         if(!Directory.Exists(path))
         {
             // 如果文件夹不存在，则新建一个
@@ -18,25 +62,37 @@ public class BuildAssetBundle : MonoBehaviour
         }
 
         // 使用LZ4压缩格式
-        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
-
-        Debug.Log("AssetBundle Build完毕:" + path);
-    }
-
-    [MenuItem("HP Utils/AssetBundle/Clear")]
-    static void ClearBuildAssetBundles()
-    {
-        // ab资源存储目录
-        string path = Application.dataPath.Replace("/Assets", "/AssetBundles/PC");
-        if (!Directory.Exists(path))
+        switch(assetBundlePlatform)
         {
-            // 如果文件夹不存在，则新建一个
-            Directory.CreateDirectory(path);
+            case AssetBundlePlatform.PC:
+                {
+                    BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+                    break;
+                }
+
+            case AssetBundlePlatform.Android:
+                {
+                    BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+                    break;
+                }
+
+            case AssetBundlePlatform.iOS:
+                {
+                    BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.iOS);
+                    break;
+                }
         }
 
-        // 使用LZ4压缩格式
-        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+        Debug.Log("AssetBundle.BuildAssetBundles " + assetBundlePlatform + " 完毕:" + path);
+    }
 
-        Debug.Log("AssetBundle Build完毕:" + path);
+    static void ClearBuildAssetBundles(AssetBundlePlatform assetBundlePlatform)
+    {
+        string path = Application.dataPath.Replace("/Assets", "/AssetBundles/" + assetBundlePlatform.ToString());
+        DirectoryInfo di = new DirectoryInfo(path);
+        di.Delete(true);
+
+        //Directory.Delete(path);
+        Debug.Log("AssetBundle ClearBuildAssetBundles " + assetBundlePlatform + " 完毕:" + path);
     }
 }
